a2b4ae6e2928a77e0339b140ceea2a6faf2128e7
Jingwei Ni <jni@wencomine.com>
2023-07-23 14:21:53 +1000

    Added multiple responses

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod pid_responses;
 use anyhow::Context;
 use embedded_can::{nb::Can, Frame as EmbeddedFrame, StandardId};
 use nb::block;
-use pid_responses::parse_pid_responses;
+use pid_responses::{parse_pid_responses, PidResponses};
 use socketcan::{CanFrame, CanSocket, Frame, Socket};
 use std::collections::HashMap;
 use std::env;
@@ -12,24 +12,50 @@ use std::env;
 extern crate lazy_static;
 
 lazy_static! {
-    static ref PID_RESPONSES: HashMap<u8, Vec<u8>> = parse_pid_responses().unwrap();
+    static ref PID_RESPONSES: HashMap<u8, PidResponses> = parse_pid_responses().unwrap();
 }
 
-fn response_frame(frame: &CanFrame) -> Option<CanFrame> {
-    println!("Processing: {}", frame_to_string(frame));
+fn response_frame(
+    frame: &CanFrame,
+    pid_responses_index: &mut HashMap<u8, u32>,
+) -> Option<CanFrame> {
+    // Check broadcast id
     if frame.raw_id() != 0x7df {
         return None;
     }
+    // Check service is 01
     let service = frame.data()[1];
     if service != 1 {
         return None;
     }
-    let pid = frame.data()[2];
-    let response = &PID_RESPONSES[&pid];
-    let frame = CanFrame::new(StandardId::new(0x7e8).unwrap(), &response)
-        .expect("Creating response can frame");
+    let pid: u8 = frame.data()[2];
+    if let Some(responses_info) = PID_RESPONSES.get(&pid) {
+        // ECU Id
+        let id = StandardId::new(0x7e8).unwrap();
 
-    Some(frame)
+        // Count service and pid with num_data_bytes
+        // Add 0x40 to service: https://en.wikipedia.org/wiki/OBD-II_PIDs#Response
+        let mut data: Vec<u8> = vec![
+            responses_info.num_data_bytes as u8 + 2u8,
+            service + 0x40,
+            pid,
+        ];
+        if responses_info.responses.len() == 1 {
+            data.extend_from_slice(&responses_info.responses[0]);
+            data.push(0); // Last data byte is part of the standard can frame but unused.
+            CanFrame::new(id, &data)
+        } else {
+            let i = pid_responses_index[&pid];
+            data.extend_from_slice(&responses_info.responses[i as usize]);
+            data.push(0); // Last data byte is part of the standard can frame but unused.
+            *pid_responses_index
+                .get_mut(&pid)
+                .expect("Couldn't retrieve pid responses index") = i + 1;
+            CanFrame::new(id, &data)
+        }
+    } else {
+        None
+    }
 }
 
 fn main() -> anyhow::Result<()> {
@@ -39,37 +65,17 @@ fn main() -> anyhow::Result<()> {
     sock.set_nonblocking(true)
         .context("Failed to make socket non-blocking")?;
 
+    let mut pid_responses_index: HashMap<u8, u32> = HashMap::new();
+    for pid in PID_RESPONSES.keys() {
+        pid_responses_index.insert(*pid, 0u32);
+    }
+
     loop {
         let received_frame = block!(sock.receive()).context("Receiving frame")?;
-        if let Some(transmit_frame) = response_frame(&received_frame) {
+        if let Some(transmit_frame) = response_frame(&received_frame, &mut pid_responses_index) {
             block!(sock.transmit(&transmit_frame)).context("Transmitting frame")?;
         } else {
-            eprintln!(
-                "No response for frame: {}",
-                frame_to_string(&received_frame)
-            );
+            eprintln!("No response for pid: {}", received_frame.data()[2]);
         }
     }
 }
-
-fn frame_to_string<F: Frame>(frame: &F) -> String {
-    let id = frame.raw_id();
-    let data_string = frame
-        .data()
-        .iter()
-        .fold(String::from(""), |a, b| format!("{} {:02x}", a, b));
-
-    format!("{:X}  [{}] {}", id, frame.dlc(), data_string)
-}
-
-// #[cfg(test)]
-// mod test {
-//     use super::*;
-//     #[test]
-//     fn test_process_can_frame_can_id() {
-//         let frame1 = CanFrame::new(StandardId::new(0x7df).unwrap(), &[1, 2, 3, 4]).unwrap();
-//         let frame2 = CanFrame::new(StandardId::new(0x1f1).unwrap(), &[1, 2, 3, 4]).unwrap();
-//         assert_eq!(respond_to_can_frame(&frame1), None);
-//         assert_eq!(!respond_to_can_frame(&frame2), Some());
-//     }
-// }
--- src/pid_responses.rs
+++ src/pid_responses.rs
@@ -1,12 +1,53 @@
 use anyhow;
+use serde::{Deserialize, Serialize};
 use serde_yaml;
 use std::collections::HashMap;
 use std::fs::File;
 
 static FILE: &str = "pid_responses.yaml";
 
-pub fn parse_pid_responses() -> anyhow::Result<HashMap<u8, Vec<u8>>> {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct PidResponses {
+    pub num_data_bytes: usize,
+    pub responses: Vec<Vec<u8>>,
+}
+
+pub fn parse_pid_responses() -> anyhow::Result<HashMap<u8, PidResponses>> {
     let reader = File::open(FILE)?;
-    let yaml: HashMap<u8, Vec<u8>> = serde_yaml::from_reader(reader)?;
+    let yaml: HashMap<u8, PidResponses> = serde_yaml::from_reader(reader)?;
     Ok(yaml)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_test() {
+        let responses = parse_pid_responses().unwrap();
+        assert_eq!(responses.len(), 10);
+
+        // check periodic parameters contain 245 responses
+        for (pid, response_info) in responses.iter() {
+            if (0..=1).contains(pid) {
+                assert_eq!(response_info.responses.len(), 1);
+            } else {
+                assert_eq!(response_info.responses.len(), 245);
+            }
+        }
+
+        // check pid 0 contains correct supported flagss
+        let pid0_data = &responses[&0].responses[0];
+        let pid0_bits = (pid0_data[0] as u32) << 8 * 3 | (pid0_data[1] as u32) << 8 * 2;
+        const BIT_MASK: u32 = 0x80000000;
+        for i in 0..32 {
+            if ((pid0_bits << i) & BIT_MASK) == BIT_MASK {
+                if i + 1 == 2 {
+                    // special case
+                    continue;
+                }
+                assert!(responses.get(&(i + 1)).is_some());
+            }
+        }
+    }
 }
